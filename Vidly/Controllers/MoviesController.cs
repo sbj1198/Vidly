4a60b585fb88dcbe4ff404cf4266af1c42e66101
biz036 <biz036@BIZ-022>
2023-04-28 11:59:18 +0530

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            
+            return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+    }
+}
